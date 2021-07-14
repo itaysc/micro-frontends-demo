1bec37dd8429a959bbad98771840cbba34d32445
itay_sc <itays@realcommerce.co.il>
2021-07-15 00:28:02 +0300

    navigation changes in mfe

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,12 +1,22 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
-
+import { useHistory } from 'react-router-dom';
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathName })=>{
+        if(history.location.pathname !== nextPathName){
+          history.push(nextPathName);
+        }
+      }
+    });
+
+    // let the child mfe know about navigation in the container app
+    history.listen(onParentNavigate); 
+  }, []);
 
   return <div ref={ref} />;
 };
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { createMemoryHistory, createBrowserHistory } from 'history';
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  // in a shared mfe we want to use memory history and not browser history
+  // we will use browser history only in the container project so only the container will change 
+  // the url, and all routing inside the shared mfe will be seamless to the user and will not change
+  // the actual location (e.g like in the gmail web app)
+  const history = defaultHistory || createMemoryHistory();
+  if(onNavigate){
+    history.listen(onNavigate); // onNavigate iss a callback supplied by the container app
+  }
+  ReactDOM.render(<App history={history}/>, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }){
+      if(history.location.pathname !== nextPathName){
+        history.push(nextPathName);
+      }
+    }
+  }
 };
 
 // If we are in development and in isolation,
@@ -13,7 +29,8 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    // in dev isolation mode we want to have a browser history
+    mount(devRoot, {defaultHistory: createBrowserHistory()});
   }
 }
 
